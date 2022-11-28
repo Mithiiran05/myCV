5be1de50317bcb841770b0f80f6579c42f063338
Mithiiran <mithiiran@student.usm.my>
2022-11-28 23:24:33 +0800

    url

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <div className="app">
-      <Router basename={'myCV'}>
+      <Router basename={'/myCV'}>
         <ScrollToTop/>
         <Switch>
           <Route path={"${process.env.PUBLIC_URL}/"} exact component={Main} />
